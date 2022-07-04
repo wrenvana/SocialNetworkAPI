46bd1b99a88df01e7bab103bb8bce7d71eae655e
Wren Sanchez <demisa00@gmail.com>
2022-07-04 13:28:50 -0700

    Initial file setup.

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const cwd = process.cwd();
+
+const app = express();
+const PORT = process.env.port || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+db.once("open", () => {
+    app.listen(PORT, () => {
+    console.log(`Now listening!`);
+    });
+});
