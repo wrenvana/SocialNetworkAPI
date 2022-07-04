8733aa010307efcb625400039a1793211b7f55a5
Wren Sanchez <demisa00@gmail.com>
2022-07-04 13:52:25 -0700

    Added controllers.

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const db = require("./config/connection");
 const routes = require("./routes");
 
-const cwd = process.cwd();
-
 const app = express();
 const PORT = process.env.port || 3001;
 
