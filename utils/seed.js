22e7b3f59eca73e7b47ac1407ce3d605c5ec7485
Wren Sanchez <demisa00@gmail.com>
2022-07-06 21:47:32 -0700

    Some minor edits and added to seeds.js file.

--- utils/seed.js
+++ utils/seed.js
@@ -5,4 +5,22 @@ connection.on('error', (err) => err);
 
 connection.once('open', async () => {
     console.log('connected');
+    await User.deleteMany({});
+    await Thought.deleteMany({});
 });
+
+//TODO: Populate users and thoughts - do I need an array for reactions?
+const users = [];
+const thoughts = [];
+
+await Thought.collection.insertMany(thoughts);
+    thoughts.forEach((thought) => {
+    users.forEach((user) => {
+    if (thought.username === user.username) {
+        user.thoughts.push(thought._id);
+    }
+    });
+});
+
+
+await User.collection.insertMany(users);
