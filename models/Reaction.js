349cb8881117dbc970fda0a99b15fcb092fe13ff
Wren Sanchez <demisa00@gmail.com>
2022-07-06 21:26:19 -0700

    Added thought and reaction models.

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,1 +1,42 @@
-const { Schema, Types } = require("mongoose");
+//Is this meant to be part of the thought model and not it's own model?
+
+
+const { Schema, model } = require("mongoose");
+
+const reactionSchema = new Schema(
+    {
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+    },
+
+    username: {
+        type: String,
+        required: true,
+    },
+
+    createdAt: {
+        type: String, 
+        default: Date.now,
+        //TODO: Add timestamps
+    },
+    },
+
+    {
+    toJSON: {
+        getters: true,
+    },
+        _id: false,
+    }
+);
+
+const Reaction = model("reaction", reactionSchema);
+
+module.exports = reactionSchema;
+
--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,42 @@
-const { Schema, Types } = require("mongoose");
+const { Schema, model } = require("mongoose");
+const reactionSchema = require('./Reaction');
+
+const thoughtsSchema = new Schema({
+    thoughtText: { 
+        type: String, 
+        required: true,
+        minLength: 1,
+        maxLength: 280,
+    },
+
+    createdAt: { 
+        type: String, 
+        default: Date.now,
+        //TODO: Add timestamps
+    },
+
+    username:{ 
+        type: String, 
+        required: true 
+    },
+
+    reactions:[reactionSchema]
+    },
+    {
+        toJSON: {
+        virtuals: true,
+        },
+        id: false,
+    }
+);
+
+
+thoughtsSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return this.reactions.length;
+    });
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = thoughtsSchema;
