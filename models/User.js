15a28b44a26a6e7897964fe42d3ab35c51e10820
Wren Sanchez <demisa00@gmail.com>
2022-07-06 21:16:34 -0700

    Drafted user model.

--- models/User.js
+++ models/User.js
@@ -1,1 +1,47 @@
-const { Schema, Types } = require("mongoose");
+const { Schema, model } = require("mongoose");
+const thoughtsSchema = require('./Thought');
+
+const validEmail = function validateEmail(mail) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value)) {
+    return true;
+    }
+    alert("Please submit a valid email address.");
+    return false;
+}
+
+const userSchema = new Schema(
+{
+    username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    },
+    email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [validEmail],
+    },
+    thoughts:[
+    { type: Schema.Types.ObjectId, 
+        ref: 'Thought' 
+    }],
+    friends: [
+    { type: Schema.Types.ObjectId, 
+        ref: 'User' 
+    }],
+    toJSON: {
+    virtuals: true,
+    },
+}
+);
+
+userSchema.virtual("friendCount")
+    .get(function () {
+    return this.friends.length;
+    });
+
+const User = model("user", userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require("express");
 const db = require("./config/connection");
 const routes = require("./routes");
 
-const app = express();
 const PORT = process.env.port || 3001;
+const app = express();
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json());
 app.use(routes);
 
 db.once("open", () => {
